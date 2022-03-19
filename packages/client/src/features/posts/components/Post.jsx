5331eab1a1635c190156222fbde2d2eaa71fc986
Steve Smodish <smodish@hotmail.com>
2022-03-19 17:16:11 -0400

    include react-router react-router-dom and styled-components, add terrible style to Post component

--- packages/client/src/features/posts/components/Post.jsx
+++ packages/client/src/features/posts/components/Post.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const PostCard = styled.div`
+  border: 1px solid orange;
+  border-radius: 5px;
+  margin-bottom: 6px;
+  padding: 3px;
+`
 
 const Post = () => {
   return (
-    <div>
+    <PostCard>
       <h3>Post Title</h3>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias voluptatum harum repudiandae? Cupiditate
         maiores non animi accusamus saepe vitae incidunt! Neque, eligendi ipsam! Dolores, tempore facere nam expedita
         nesciunt reprehenderit!
       </p>
-    </div>
+    </PostCard>
   )
 }
 
