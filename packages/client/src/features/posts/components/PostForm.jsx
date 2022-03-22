94ed1997b431faf56ba7c980278b7d3eadb80dac
Steve Smodish <smodish@hotmail.com>
2022-03-21 20:54:31 -0400

    PostForm posting to firebase

--- packages/client/src/features/posts/components/PostForm.jsx
+++ packages/client/src/features/posts/components/PostForm.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
+import { collection, addDoc } from 'firebase/firestore'
+
+import { db } from '../../../config/firebase.ts'
+
+const initialState = {
+  title: '',
+  post: '',
+}
 
 const PostForm = () => {
-  const [formData, setFormData] = useState({
-    title: '',
-    post: '',
-  })
+  const [formData, setFormData] = useState(initialState)
 
   const { title, post } = formData
 
@@ -12,10 +17,27 @@ const PostForm = () => {
     setFormData((prevState) => ({ ...prevState, [e.target.name]: e.target.value }))
   }
 
+  const onSubmit = async (e) => {
+    // TODO: submit data to firebase
+    e.preventDefault()
+    try {
+      const docRef = await addDoc(collection(db, 'posts'), {
+        ...formData,
+        uid: 99,
+      })
+      console.log('Document written with ID: ', docRef.id)
+    } catch (e) {
+      console.error('Error adding document: ', e)
+    }
+
+    setFormData(initialState)
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <input type='text' name='title' onChange={onChange} value={title} />
       <input type='text' name='post' onChange={onChange} value={post} />
+      <button type='submit'>Submit</button>
     </form>
   )
 }
