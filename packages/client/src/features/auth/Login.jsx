e3e196330fc92205ffee272e6bd8e476078f77f8
Steve Smodish <smodish@hotmail.com>
2022-03-28 20:48:36 -0400

    login user is working

--- packages/client/src/features/auth/Login.jsx
+++ packages/client/src/features/auth/Login.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
 const auth = getAuth()
+const initialState = {
+  email: '',
+  password: '',
+}
 
 function Login() {
+  const [formData, setFormData] = useState(initialState)
+
+  const { email, password } = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({ ...prevState, [e.target.name]: e.target.value }))
+  }
+
   async function handleLogin(event) {
     event.preventDefault()
-    const {
-      email: { value: email },
-      password: { value: password },
-    } = event.target.elements
+    // const {
+    //   email: { value: email },
+    //   password: { value: password },
+    // } = event.target.elements
 
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
@@ -23,12 +35,15 @@ function Login() {
         const errorMessage = error.message
         console.log(errorCode, errorMessage)
       })
+
+    // Clearing inputs after successful signup
+    setFormData(initialState)
   }
 
   return (
     <form onSubmit={handleLogin} className='loginForm'>
-      <input type='email' name='email' placeholder='Email' />
-      <input type='password' name='password' placeholder='Password' />
+      <input type='email' name='email' placeholder='Email' onChange={onChange} value={email} />
+      <input type='password' name='password' placeholder='Password' onChange={onChange} value={password} />
       <button type='submit'>Login</button>
     </form>
   )
