4fb2ca02692869ec4f0aa7d0b8008e97e9fb388b
Steve Smodish <smodish@hotmail.com>
2022-04-26 14:49:00 -0400

    profile and about pages, some styling

--- src/features/about/About.jsx
+++ src/features/about/About.jsx
@@ -5,19 +5,42 @@ const About = () => {
     <div>
       <h1>About</h1>
       <p>
-        {`A meetup for new Devs pursuing their first Dev job, those leveling up their skills for mid-level positions, or
-        just starting in programming looking for a career switch - Although all are welcome to join the conversation!
-        Our goals for Grammerhub's weekly Monday coding nights*: - Support independent projects and collaboration -
-        Build a community and provide accountability - Provide opportunities to grow professionally with technology Who
-        we are: A grassroots group of volunteers applying our skills in technology, design, research, and more to
-        support those looking to reach the next level in their Dev careers. How it works: - 6:30 pm EST: We open our
-        general video conference call (Intros, Q&A, Show-and-Tell, etc.) - 7:00 pm EST: Work on Project(s) - 8:15 pm
-        EST: Wrap-up, announcements New to Grammerhub? No problem! We have volunteer organizers to help you get started.
-        Grammerhub workspace on Slack: http://bit.ly/grammerhub-slack What you’ll learn: - Web development techniques
-        and syntax for programming in multiple languages - How to deploy your code - Resources for continued learning
-        Looking forward to working with you all! - The Grammerhub Team _ *Going strong every Monday since March 9th,
-        2020`}
+        Grammerhub is a meetup for new Devs pursuing their first Dev job, those leveling up their skills for mid-level
+        positions, or just starting in programming looking for a career switch -
+        <span>Although all are welcome to join the conversation!</span>
       </p>
+      <p>
+        Our goals for Grammerhub&#39;s weekly Monday coding nights*:
+        <ul>
+          <li>Support independent projects and collaboration</li>
+          <li>Build a community and provide accountability</li>
+          <li>Provide opportunities to grow professionally with technology</li>
+        </ul>
+      </p>
+      <p>
+        Who We Are: A grassroots group of volunteers applying our skills in technology, design, research, and more to
+        support those looking to reach the next level in their Dev careers.
+      </p>
+      <p>
+        How it works:
+        <ul>
+          <li>6:30 pm EST: We open our general video conference call (Intros, Q&A, Show-and-Tell, etc.)</li>
+          <li>7:00 pm EST: Work on Project(s)</li>
+          <li>8:15 pm EST: Wrap-up, announcements</li>
+        </ul>
+      </p>
+      <p>New to Grammerhub? No problem! We have volunteer organizers to help you get started.</p>
+      <p>Grammerhub workspace on Slack: http://bit.ly/grammerhub-slack</p>
+      <p>
+        What you&#39;ll learn:
+        <ul>
+          <li>Web development techniques and syntax for programming in multiple languages</li>
+          <li>How to deploy your code</li>
+          <li>Resources for continued learning</li>
+        </ul>
+      </p>
+      <p>Looking forward to working with you all!</p>
+      <h3>The Grammerhub Team _ *Going strong every Monday since March 9th, 2020*</h3>
     </div>
   )
 }
--- src/features/posts/components/styles/Tag.styled.js
+++ src/features/posts/components/styles/Tag.styled.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components'
 
 export const Tag = styled.div`
-  background-color: #333;
+  font-size: 0.8rem;
+  background-color: rgba(30, 30, 30, 0.8);
   color: #ccc;
   padding: 3px 5px;
-  border-radius: 5px;
+  border-radius: 3px;
   margin-top: 2px;
   margin-right: 3px;
   margin-bottom: 2px;
