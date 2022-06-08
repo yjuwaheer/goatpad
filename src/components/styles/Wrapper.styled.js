ec7bbe88b0474348efd4eff5efa1e613e9db293d
yjuwaheer <yjuwaheer@mun.ca>
2022-06-07 22:04:43 -0230

    Refactored code

--- src/components/styles/Wrapper.styled.js
+++ src/components/styles/Wrapper.styled.js
@@ -1,4 +1,4 @@
-// import React from 'react'
+// Import styles components
 import styled from 'styled-components'
 
 // Global container that wraps the whole app
@@ -15,72 +15,3 @@ export const GlobalContainer = styled.div`
     color: black;
   }
 `
-
-// Create Post Container
-export const CreatePostContainer = styled.div`
-  display: flex;
-  justify-content: center;
-
-  form {
-    display: flex;
-    flex-direction: column;
-    width: 50vw;
-    background-color: #fafafa;
-    padding: 1rem;
-    border-radius: 5px;
-
-    label {
-      font-size: 1.2rem;
-      font-weight: bold;
-    }
-
-    input {
-      padding: 0 0.5rem;
-      border-radius: 5px;
-      border: 3px solid rgba(30, 30, 100, 0.3);
-
-      &:focus {
-        outline: none;
-        border: 3px solid rgba(30, 30, 100, 0.8);
-      }
-    }
-
-    textarea {
-      resize: vertical;
-      height: 100px;
-      padding: 0 0.5rem;
-      border-radius: 5px;
-      border: 3px solid rgba(30, 30, 100, 0.3);
-
-      &:focus {
-        outline: none;
-        border: 3px solid rgba(30, 30, 100, 0.8);
-      }
-    }
-
-    p {
-      color: rgba(207, 40, 40, 1);
-    }
-
-    button {
-      width: 200px;
-      justify-self: center;
-      padding: 0.2rem;
-      background-color: rgba(30, 30, 100, 0.5);
-      border-radius: 5px;
-      border: 3px solid rgba(30, 30, 100, 0.3);
-      color: white;
-
-      &:hover {
-        cursor: pointer;
-        background-color: rgba(30, 30, 100, 0.7);
-      }
-    }
-
-    @media (max-width: 1000px) {
-      width: 90vw;
-    }
-  }
-
-`
-
--- src/features/posts/components/PostForm.jsx
+++ src/features/posts/components/PostForm.jsx
@@ -3,12 +3,79 @@ import { useNavigate } from 'react-router'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { postFormSchema } from './postFormSchema'
+import styled from 'styled-components'
 
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore'
 import { db } from '../../../config/firebase.ts'
 import { useAuthContext } from '../../../hooks/useAuthContext'
 import { Stack } from '../../../components/styles/Stack.styled'
-import { CreatePostContainer } from '../../../components/styles/Wrapper.styled'
+
+// Styled components
+const CreatePostContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 50vw;
+  background-color: #fafafa;
+  padding: 1rem;
+  border-radius: 5px;
+
+  @media (max-width: 1000px) {
+    width: 90vw;
+  }
+`
+
+const Label = styled.label`
+  font-size: 1.2rem;
+  font-weight: bold;
+`
+
+const Input = styled.input`
+  padding: 0 0.5rem;
+  border-radius: 5px;
+  border: 3px solid rgba(30, 30, 100, 0.3);
+
+  &:focus {
+    outline: none;
+    border: 3px solid rgba(30, 30, 100, 0.8);
+  }
+`
+
+const Textarea = styled.textarea`
+  resize: vertical;
+  height: 100px;
+  padding: 0 0.5rem;
+  border-radius: 5px;
+  border: 3px solid rgba(30, 30, 100, 0.3);
+
+  &:focus {
+    outline: none;
+    border: 3px solid rgba(30, 30, 100, 0.8);
+  }
+`
+
+const Error = styled.p`
+  color: rgba(207, 40, 40, 1);
+`
+
+const Button = styled.button`
+  width: 200px;
+  justify-self: center;
+  padding: 0.2rem;
+  background-color: rgba(30, 30, 100, 0.5);
+  border-radius: 5px;
+  border: 3px solid rgba(30, 30, 100, 0.3);
+  color: white;
+
+  &:hover {
+    cursor: pointer;
+    background-color: rgba(30, 30, 100, 0.7);
+  }
+`
 
 const PostForm = () => {
   const {
@@ -48,26 +115,26 @@ const PostForm = () => {
 
   return (
     <CreatePostContainer>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Stack gutter='lg'>
           <Stack gutter='sm'>
-            <label htmlFor='title'>Title</label>
-            <input type='text' name='title' {...register('title')} placeholder='Enter a title' />
-            <p>{errors.title?.message}</p>
+            <Label htmlFor='title'>Title</Label>
+            <Input type='text' name='title' {...register('title')} placeholder='Enter a title' />
+            <Error>{errors.title?.message}</Error>
           </Stack>
           <Stack gutter='sm'>
-            <label htmlFor='title'>Topics</label>
-            <input type='text' name='topics' {...register('topics')} placeholder='Enter topics separated by commas' />
-            <p>{errors.title?.message}</p>
+            <Label htmlFor='title'>Topics</Label>
+            <Input type='text' name='topics' {...register('topics')} placeholder='Enter topics separated by commas' />
+            <Error>{errors.title?.message}</Error>
           </Stack>
           <Stack gutter='sm'>
-            <label htmlFor='postBody'>Body</label>
-            <textarea name='postBody' {...register('postBody')} placeholder='Enter your post' rows='6' />
-            <p>{errors.postBody?.message}</p>
+            <Label htmlFor='postBody'>Body</Label>
+            <Textarea name='postBody' {...register('postBody')} placeholder='Enter your post' rows='6' />
+            <Error>{errors.postBody?.message}</Error>
           </Stack>
-          <button type='submit'>Create Post</button>
+          <Button type='submit'>Create Post</Button>
         </Stack>
-      </form>
+      </Form>
     </CreatePostContainer>
   )
 }
