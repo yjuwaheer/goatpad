3af48a60d0414920047a764c9a793df13a4ac880
yjuwaheer <yjuwaheer@mun.ca>
2022-06-09 17:02:27 -0230

    Updated profile

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,25 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyDNBGACzF08f2BnFUCWJS5xtgnHUNPSnOM',
-//   authDomain: 'goat-pad.firebaseapp.com',
-//   projectId: 'goat-pad',
-//   storageBucket: 'goat-pad.appspot.com',
-//   messagingSenderId: '390770819378',
-//   appId: '1:390770819378:web:c2b8561137a05807cb972b',
-//   measurementId: 'G-ZCX7K31E0W',
-// }
-
-// Test firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyD8X5Sr2KvJXuVSVLJvN3t7jSKQmts3xyE",
-  authDomain: "testapp-d3881.firebaseapp.com",
-  projectId: "testapp-d3881",
-  storageBucket: "testapp-d3881.appspot.com",
-  messagingSenderId: "8869824824",
-  appId: "1:8869824824:web:6fe012efc30b0430f619cd",
-  measurementId: "G-1YE52QSCCD",
+  apiKey: "AIzaSyDNBGACzF08f2BnFUCWJS5xtgnHUNPSnOM",
+  authDomain: "goat-pad.firebaseapp.com",
+  projectId: "goat-pad",
+  storageBucket: "goat-pad.appspot.com",
+  messagingSenderId: "390770819378",
+  appId: "1:390770819378:web:c2b8561137a05807cb972b",
+  measurementId: "G-ZCX7K31E0W",
 };
 
 // Initialize Firebase
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 
 import { useAuthContext } from "../hooks/useAuthContext";
+import styled from "styled-components";
+
+const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Span = styled.span`
+  color: rgba(30, 30, 100, 0.8);
+`;
 
 const Profile = () => {
   const { user } = useAuthContext();
 
   return (
-    <div>
-      <h1>{user.displayName}</h1>
-      <h2>{user.email}</h2>
-    </div>
+    <ProfileContainer>
+      <h1>Username: <Span>{user.displayName}</Span></h1>
+      <h2>Email: <Span>{user.email}</Span></h2>
+    </ProfileContainer>
   );
 };
 
